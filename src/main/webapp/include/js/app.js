30daf37ab6c5e697d86c6a5302e2649243c4ec4c
DzenoBa <DzenoBa@users.noreply.github.com>
2015-02-18 17:48:05 +0100

    +app.js

---
+++ src/main/webapp/include/js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* 
+ *  The Bokforing App
+ */
+var bok = angular.module('Bok', [
+    'ngRoute',
+    'AuthControllers',
+    'AuthService'
+     // More here
+]);
+
+
+bok.config(['$routeProvider',
+    function($routeProvider) {  // Injected object $routeProvider
+        $routeProvider.
+                when('/login', {
+                    templateUrl: 'login.html',
+                    controller: 'LoginCtrl'
+                }).
+                when('/userpage', {
+                    templateUrl: 'userpage.html',
+                    controller: 'UserPageCtrl'
+                }).
+                otherwise({
+                    redirectTo: '/index.html'
+                });
+
+    }]);
+
+
