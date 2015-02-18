b5fe0dc9886bd876d08fc5023ca537c8a9e5a106
DzenoBa <DzenoBa@users.noreply.github.com>
2015-02-18 17:36:59 +0100

    +FormJSON

---
+++ src/main/java/se/chalmers/bokforing/jsonobject/FormJSON.java
@@ -0,0 +1,37 @@
+
+package se.chalmers.bokforing.jsonobject;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Dženan
+ */
+public class FormJSON implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+        
+    private int numErrors;
+    private HashMap<String, String> errors;
+
+    public FormJSON() {
+        numErrors = 0;
+        errors = new HashMap<String, String>();
+    }
+
+    public int getNumErrors() {
+        return numErrors;
+    }
+    public void increaseErrors() {
+        numErrors = numErrors+1;
+    }
+
+    public HashMap<String, String> getErrors() {
+        return errors;
+    }
+    public void addError(String name, String message) {
+        errors.put(name, message);
+        increaseErrors();
+    }
+}
