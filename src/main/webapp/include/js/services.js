dad90b2cb2f12348e8e8d35be34034efcdddebae
DzenoBa <DzenoBa@users.noreply.github.com>
2015-02-18 17:44:41 +0100

    +services.js

---
+++ src/main/webapp/include/js/services.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/* 
+ * Services 
+ * 
+ * @author Dženan
+ */
+
+var authService = angular.module('AuthService', []);
+
+authService.factory('AuthProxy', ['$http',
+    function($http) {
+       var url = 'http://localhost:8080/bokforing/auth';
+        return {
+            login: function(user) {
+                return $http.post(url + '/login', user);
+            },
+            status: function() {
+                return $http.get(url + '/status');
+            },
+            get: function() {
+                return $http.get(url + '/get');
+            },
+            logout: function() {
+                return $http.get(url + '/logout');
+            }
+        };
+    }]);
