d9f07fe9a9f73f5f6501c1ae4a1ee97400f6e188
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2015-02-20 12:30:10 +0100

    Added database configuration class.

--- src/main/java/se/chalmers/bokforing/springstuff/WebConfig.java
+++ src/main/java/se/chalmers/bokforing/springstuff/WebConfig.java
@@ -8,6 +8,7 @@ package se.chalmers.bokforing.springstuff;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
