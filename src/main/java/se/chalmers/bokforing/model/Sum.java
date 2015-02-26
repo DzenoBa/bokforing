f573aa2092469af43ce544f8ce319be0cf2a01f3
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2015-02-26 19:06:01 +0100

    Changed Sum value to double from int. Should probably be able to make floating-point transactions.

--- src/main/java/se/chalmers/bokforing/model/Sum.java
+++ src/main/java/se/chalmers/bokforing/model/Sum.java
@@ -24,12 +24,12 @@ public class Sum implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     
-    private int value;
+    private double value;
     
     @Enumerated(EnumType.STRING)
     private PostType type;
     
-    public Sum(int value, PostType type){
+    public Sum(double value, PostType type){
         this.value = value;
         this.type = type;
     }
