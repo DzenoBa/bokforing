d171e45ceed076d5611348ca906e8709c5bc6d7a
DzenoBa <DzenoBa@users.noreply.github.com>
2015-02-27 17:20:26 +0100

    + edit-user

--- src/main/java/se/chalmers/bokforing/controller/UserController.java
+++ src/main/java/se/chalmers/bokforing/controller/UserController.java
@@ -3,14 +3,17 @@ package se.chalmers.bokforing.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import se.chalmers.bokforing.helperfunctions.HelpY;
 import se.chalmers.bokforing.jsonobject.FormJSON;
+import se.chalmers.bokforing.jsonobject.UserJSON;
 import se.chalmers.bokforing.model.Group;
 import se.chalmers.bokforing.service.UserService;
 import se.chalmers.bokforing.model.UserAccount;
+import se.chalmers.bokforing.session.AuthSession;
 
 /**
  * USER-CONTROLLER
@@ -23,6 +26,9 @@ public class UserController {
 
     @Autowired
     private UserService userDb;
+    
+    @Autowired 
+    private AuthSession authSession;
     
     HelpY helpy = new HelpY();
     
@@ -56,5 +62,52 @@ public class UserController {
             
             return form;
         }
+    }
+    
+    @RequestMapping(value = "/user/edit", method = RequestMethod.POST)
+    public @ResponseBody FormJSON edit(@RequestBody final UserJSON user) {
+        System.out.println("* PING user/edit");
+        FormJSON form = new FormJSON();
+        
+        // PASSWORD CHECK
+        if(user.getNewpasswd() == null || user.getNewpasswd().isEmpty()) {
+            form.addError("newpasswd", "Vänligen ange ett lösenord!");
+            return form;
+        }
+        if(user.getNewpasswd2() == null || user.getNewpasswd2().isEmpty()) {
+            form.addError("newpasswd2", "Vänligen ange ett lösenord!");
+            return form;
+        }
+        if(!(user.getNewpasswd().equals(user.getNewpasswd2()))) {
+            form.addError("newpasswd2", "Lösenordet matchar inte!");
+            return form;
+        }
+        
+        String email = authSession.getEmail();
+        if(email == null || email.isEmpty()) {
+            form.addError("general", "Något gick fel, vänligen försök igen om en liten stund!");
+            return form;
+        }
+        UserAccount u = userDb.getUser(email);
+        if(u == null) {
+            form.addError("general", "Något gick fel, vänligen försök igen om en liten stund!");
+            return form;
+        }
+        
+        // CHECK IF VALID PASSWORD
+        String hashPasswd = helpy.hash(u.getSalt() + user.getPasswd());
+        if(!(hashPasswd.equals(u.getPass()))) {
+            form.addError("passwd", "Löseordet är fel!");
+            return form;
+        }
+        
+        // EVERYTHING SEEMS TO BE IN ORDER CHANGE USER
+        String newSalt = helpy.randomString(8);
+        String newHashPasswd = newSalt + user.getNewpasswd(); // TODO; hash it here or in UserDB
+        u.setSalt(newSalt);
+        u.setPass(newHashPasswd);
+        userDb.storeUser(u);
+        
+        return form;
     }
 }
