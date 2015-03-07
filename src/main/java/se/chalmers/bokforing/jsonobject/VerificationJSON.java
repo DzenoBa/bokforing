52f07ff1cb6670c799f7f16539a73be9af858708
DzenoBa <DzenoBa@users.noreply.github.com>
2015-03-07 15:46:28 +0100

    + VerificationJSON.java

---
+++ src/main/java/se/chalmers/bokforing/jsonobject/VerificationJSON.java
@@ -0,0 +1,30 @@
+
+package se.chalmers.bokforing.jsonobject;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Dženan
+ */
+public class VerificationJSON implements Serializable {
+    
+    private List<PostJSON> posts;
+    private Date transactionDate;
+    
+    public List<PostJSON> getPosts() {
+        return posts;
+    }
+    public void setPosts(List<PostJSON> posts) {
+        this.posts = posts;
+    }
+    
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+}
