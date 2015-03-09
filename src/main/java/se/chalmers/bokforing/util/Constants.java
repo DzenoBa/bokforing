558a56b2fe45a8d178a3680c2833c87a119d4e58
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2015-03-09 11:29:56 +0100

    Constants class for keeping track of... constants.

---
+++ src/main/java/se/chalmers/bokforing/util/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.chalmers.bokforing.util;
+
+import org.springframework.data.domain.Sort;
+
+/**
+ *
+ * @author Jakob
+ */
+public class Constants {
+    
+    public static final int DEFAULT_PAGE_SIZE = 20;
+    public static final Sort.Direction DEFAULT_SORT_DIRECTION = Sort.Direction.ASC;
+}
