2c8b5127c5bee5f27572fdccf7978111e0b150a3
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2015-03-11 19:38:46 +0100

    Added plugin in maven for generating metamodel to facilitate specification queries.

---
+++ src/main/java/se/chalmers/bokforing/persistence/VerificationSpecs.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.chalmers.bokforing.persistence;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
+import se.chalmers.bokforing.model.UserAccount;
+import se.chalmers.bokforing.model.Verification;
+import se.chalmers.bokforing.model.Verification_;
+
+/**
+ *
+ * @author Jakob
+ */
+public class VerificationSpecs {
+    
+    public static Specification<Verification> hasUserAccount(final UserAccount userAccount) {
+        return new Specification<Verification>() {
+           @Override
+           public Predicate toPredicate(Root<Verification> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                return cb.equal(root.get(Verification_.userAccount), userAccount); 
+           }
+        };
+    }
+    
+}
