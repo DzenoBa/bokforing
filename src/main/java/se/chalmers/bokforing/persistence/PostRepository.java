933d787de5c176afb0363d8b67effac0c5d8d0df
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2015-03-29 15:33:49 +0200

    Added query for finding all posts that are associated with an account. For huvudbok.

---
+++ src/main/java/se/chalmers/bokforing/persistence/PostRepository.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.chalmers.bokforing.persistence;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import se.chalmers.bokforing.model.Post;
+
+/**
+ *
+ * @author Jakob
+ */
+public interface PostRepository extends JpaRepository<Post, Long> {
+    
+    @Query(""
+            + "select p "
+            + "from Post p "
+            + "where p.verification.userAccount.id = :userId "
+            + "and p.account.number = :accountNumber"
+    )
+    List<Post> findPostsForUserAndAccount(@Param("userId") long userId, @Param("accountNumber") int accountNumber);
+    
+}
