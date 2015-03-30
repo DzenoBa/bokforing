64d1cd9fc42b6d488b2786f6b6b6f24bca7aeb60
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2015-03-30 17:53:52 +0200

    Added service classes for post, this is where you get huvudbok (general ledger in english, I'm using that name).

---
+++ src/main/java/se/chalmers/bokforing/service/PostServiceImpl.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.chalmers.bokforing.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import se.chalmers.bokforing.model.Account;
+import se.chalmers.bokforing.model.Post;
+import se.chalmers.bokforing.model.user.UserAccount;
+import se.chalmers.bokforing.persistence.PostRepository;
+
+/**
+ *
+ * @author Jakob
+ */
+@Service
+@Transactional
+public class PostServiceImpl implements PostService {
+
+    @Autowired
+    private PostRepository repo;
+    
+    @Autowired
+    private AccountService accountService;
+    
+    @Override
+    public Map<Account, List<Post>> getGeneralLedger(UserAccount user) {
+        Map<Account, List<Post>> generalLedger = new HashMap<>();
+        
+        List<Account> accounts = accountService.findAllAccounts();
+        for(Account account : accounts) {
+            List<Post> posts = repo.findPostsForUserAndAccount(user.getId(), account.getNumber());
+            
+            generalLedger.put(account, posts);
+        }
+        
+        return generalLedger;
+    }
+    
+    
+    
+}
