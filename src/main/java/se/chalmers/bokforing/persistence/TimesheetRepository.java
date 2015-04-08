2790999829f1c0a1176cdd62be07d8c7712ad976
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2015-04-08 18:10:45 +0200

    Timesheet manager, service and repository.

---
+++ src/main/java/se/chalmers/bokforing/persistence/TimesheetRepository.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.chalmers.bokforing.persistence;
+
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.chalmers.bokforing.model.Timesheet;
+import se.chalmers.bokforing.model.user.UserAccount;
+
+/**
+ *
+ * @author Jakob
+ */
+@Repository
+public interface TimesheetRepository extends JpaRepository<Timesheet, Long> {
+    
+    Page<Timesheet> findByUserAccount(UserAccount userAccount, Pageable pageable);
+    
+}
---
+++ src/main/java/se/chalmers/bokforing/service/TimesheetService.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.chalmers.bokforing.service;
+
+import java.util.List;
+import org.springframework.data.domain.Page;
+import se.chalmers.bokforing.model.Timesheet;
+import se.chalmers.bokforing.model.user.UserAccount;
+import se.chalmers.bokforing.persistence.PagingAndSortingTerms;
+
+/**
+ *
+ * @author Jakob
+ */
+public interface TimesheetService {
+    
+    Timesheet findTimesheet(long id);
+    
+    Page<Timesheet> findAllTimesheets(UserAccount user, PagingAndSortingTerms terms);
+    
+    void save(Timesheet timesheet);
+    
+    void remove(Timesheet timesheet);
+    
+}
