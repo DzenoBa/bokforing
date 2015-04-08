f083820d6ef28e01eff81bbe67458fa7415ab1da
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2015-04-08 18:10:14 +0200

    @Repository tag for postrepo.

--- src/main/java/se/chalmers/bokforing/persistence/PostRepository.java
+++ src/main/java/se/chalmers/bokforing/persistence/PostRepository.java
@@ -11,12 +11,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import se.chalmers.bokforing.model.Post;
 
 /**
  *
  * @author Jakob
  */
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     
     @Query(""
