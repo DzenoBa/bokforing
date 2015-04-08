77387a436b63aefaa93de60cfea977f5dd26a272
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2015-04-08 18:09:30 +0200

    Timesheet entity.

---
+++ src/main/java/se/chalmers/bokforing/model/Timesheet.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.chalmers.bokforing.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import se.chalmers.bokforing.model.user.UserAccount;
+
+/**
+ *
+ * @author Jakob
+ */
+@Entity
+public class Timesheet implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Product product;
+    
+    @ManyToOne
+    private Customer customer;
+    
+    private Double quantity;
+    private String description;
+    
+    @Temporal(TemporalType.DATE)
+    private Date dateWorked;
+    
+    @ManyToOne
+    private UserAccount userAccount;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the product
+     */
+    public Product getProduct() {
+        return product;
+    }
+
+    /**
+     * @param product the product to set
+     */
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    /**
+     * @return the customer
+     */
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    /**
+     * @param customer the customer to set
+     */
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    /**
+     * @return the quantity
+     */
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public double getPrice() {
+        return this.getProduct().getTotal(this.getQuantity());
+    }
+
+    /**
+     * @return the dateWorked
+     */
+    public Date getDateWorked() {
+        return dateWorked;
+    }
+
+    /**
+     * @param dateWorked the dateWorked to set
+     */
+    public void setDateWorked(Date dateWorked) {
+        this.dateWorked = dateWorked;
+    }
+
+    /**
+     * @return the userAccount
+     */
+    public UserAccount getUserAccount() {
+        return userAccount;
+    }
+
+    /**
+     * @param userAccount the userAccount to set
+     */
+    public void setUserAccount(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+}
