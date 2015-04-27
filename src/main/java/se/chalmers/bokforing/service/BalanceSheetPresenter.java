4196cd1d5c0ca99f23a7237328ff47bfe939fe8a
Bella <isabellefrolich@hotmail.com>
2015-04-27 21:05:00 +0200

    90% sure that BalanceSheetPresenter is done

--- src/main/java/se/chalmers/bokforing/service/BalanceSheetPresenter.java
+++ src/main/java/se/chalmers/bokforing/service/BalanceSheetPresenter.java
@@ -10,9 +10,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -32,7 +30,6 @@ import se.chalmers.bokforing.model.user.UserAccount;
 public class BalanceSheetPresenter {
 
     private final static boolean DEBUG = true;
-    private final static DecimalFormat df = new DecimalFormat("#.##");
     Map<Account, List<Double>> balanceSheet;
 
     private Document doc;
@@ -40,27 +37,11 @@ public class BalanceSheetPresenter {
     // Maybe not correct
     private PostServiceImpl postService;
 
-//    private final UserHandler to;
-//    private final UserHandler fr;
-//    private final Content cont;
     public BalanceSheetPresenter(UserAccount user, Date startDate,
             Date endDate, Pageable pageable) {
         balanceSheet = postService.getBalanceSheet(user, startDate,
                 endDate, pageable);
-//        this.fak = faktura;
-//        to = new UserHandler(fak.getToUser());
-//        fr = new UserHandler(fak.getFromUser());
-//        cont = fak.getContent();
 
-    }
-
-    private String summaryContent() {
-        StringBuilder sb = new StringBuilder();
-        final Double momsPre = fak.getMomsPrecentage();
-        sb.append("Belopp utan moms: " + df.format(cont.getTotalPrice()) + "\n");
-        sb.append("Moms kr " + (momsPre * 100) + "% " + df.format(cont.getTotalPrice() * momsPre) + " kr\n");
-        sb.append("Belopp att betala: " + df.format(cont.getTotalPrice() + (cont.getTotalPrice() * momsPre)) + " kr");
-        return sb.toString();
     }
 
     private void replacer(String tag, String replaceWith) {
@@ -72,30 +53,54 @@ public class BalanceSheetPresenter {
     private String assetAccountsGenerator() {
         StringBuilder sb = new StringBuilder();
         Set<Account> accountSet = balanceSheet.keySet();
-        
+
         for (Account acc : accountSet) {
             if (acc.getNumber() < 2000) {
+                sb.append("<tr>");
                 sb.append("<td>").append(acc.getNumber()).append(acc.getName()).append("</td>");
                 sb.append("<td>").append(balanceSheet.get(acc).get(1)).append("</td>");
                 sb.append("<td>").append(balanceSheet.get(acc).get(0)).append("</td>");
+                sb.append("</tr>");
             }
         }
-        sb.append("</td>");
+
         return sb.toString();
     }
 
     //Generates all debtAccounts
-
     private String debtAccountsGenerator() {
         StringBuilder sb = new StringBuilder();
         Set<Account> accountSet = balanceSheet.keySet();
-        sb.append("<td>");
+
         for (Account acc : accountSet) {
             if (acc.getNumber() >= 2000) {
-                sb.append(acc.getNumber()).append(acc.getName()).append("<br>");
+                sb.append("<tr>");
+                sb.append("<td>").append(acc.getNumber()).append(acc.getName()).append("</td>");
+                sb.append("<td>").append(balanceSheet.get(acc).get(1)).append("</td>");
+                sb.append("<td>").append(balanceSheet.get(acc).get(0)).append("</td>");
+                sb.append("</tr>");
             }
         }
-        sb.append("</td>");
+
+        return sb.toString();
+    }
+
+    //Generates the result
+    private String resultGenerator() {
+        StringBuilder sb = new StringBuilder();
+        Set<Account> accountSet = balanceSheet.keySet();
+        int startingBalance = 0;
+        int totalBalance = 0;
+        for (Account acc : accountSet) {     
+            
+            startingBalance += balanceSheet.get(acc).get(1);
+            totalBalance += balanceSheet.get(acc).get(0);
+        }     
+                sb.append("<tr>");
+                sb.append("<td>").append(startingBalance).append("</td>");
+                sb.append("<td colspan=2>").append(totalBalance).append("</td>");
+                sb.append("</tr>");
+
         return sb.toString();
     }
 
@@ -122,7 +127,7 @@ public class BalanceSheetPresenter {
         replacer("skuldkonto", debtAccountsGenerator());
 
         //FINAL SECTION
-        replacer("totalcost", summaryContent());
+        replacer("ingresultat", resultGenerator());
 
         if (DEBUG) {
             System.out.println(doc.outerHtml());
