5dcae3781b485201bda87bd8636818700bb0066a
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2015-04-28 13:08:00 +0200

    Address implements Serializable.

--- src/main/java/se/chalmers/bokforing/model/Address.java
+++ src/main/java/se/chalmers/bokforing/model/Address.java
@@ -5,6 +5,7 @@
  */
 package se.chalmers.bokforing.model;
 
+import java.io.Serializable;
 import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,7 +16,7 @@ import javax.persistence.Id;
  * @author Jakob
  */
 @Entity
-public class Address {
+public class Address implements Serializable {
     
     @Id
     @GeneratedValue
